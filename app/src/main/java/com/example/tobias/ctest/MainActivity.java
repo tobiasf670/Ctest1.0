b2b5baccbd89729643283cb82dd4eccd640d6e12
Tobias <tobias-frantsen@hotmail.com>
2017-01-05 23:01:13 +0100

    Done - Need testning on it, and mabye to send text.

--- app/src/main/java/com/example/tobias/ctest/MainActivity.java
+++ app/src/main/java/com/example/tobias/ctest/MainActivity.java
@@ -65,44 +65,58 @@ public class MainActivity extends AppCompatActivity implements CircleLayout.OnIt
         svaerhedsgrad = (TextView) findViewById(R.id.TextSvaerhedsgrad);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progress = 0;
+
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
-                progress = progresValue;
-                Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+            textView.setText("Du har valgt "+String.valueOf(seekBar.getProgress())+" Minutter");
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                textView.setText("Covered: " + progress + "/" + seekBar.getMax());
-                Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+
             }
         });
 
+
+
         seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            int progress = 0;
 
             @Override
             public void onProgressChanged(SeekBar seekBar, int progresValue, boolean fromUser) {
-                progress = progresValue;
-                Toast.makeText(getApplicationContext(), "Changing seekbar's progress", Toast.LENGTH_SHORT).show();
+                // TODO Auto-generated method stub
+                switch(progresValue){
+                    case 0:
+                        textView2.setText("Du har valgt : Let øvet");
+                        break;
+                    case 1:
+                        textView2.setText("Du har valgt : Øvet");
+                        break;
+                    case 2:
+                        textView2.setText("Du har valgt : Meget øvet");
+                        break;
+                    case 3:
+                        textView2.setText("Du har valgt : Ekspert");
+                        break;
+                    default:
+                        break;
+                }
             }
+
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-                Toast.makeText(getApplicationContext(), "Started tracking seekbar", Toast.LENGTH_SHORT).show();
+
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                textView2.setText("Covered: " + progress + "/" + seekBar.getMax());
-                Toast.makeText(getApplicationContext(), "Stopped tracking seekbar", Toast.LENGTH_SHORT).show();
+
             }
         });
 
@@ -132,58 +146,152 @@ public class MainActivity extends AppCompatActivity implements CircleLayout.OnIt
     @Override
     public void onItemClick(View view) {
 
-        String name = null;
-        if(view instanceof CircleImageView){
-            name = ((CircleImageView) view).getName();
-        }
-        String text = getResources().getString(R.string.start_app, name);
-        Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
-
         switch (view.getId()) {
             case R.id.historie:
                 if (circleLayout.getChildCount() > 1) {
 
                     for (int i = 0; i < 5; i++) {
                         circleLayout.removeViewAt(circleLayout.getChildCount() - 1);
-
                     }
                 }
 
-               ImageView img = (ImageView) findViewById(R.id.historie);
+               ImageView img_his = (ImageView) findViewById(R.id.historie);
                 circleLayout.setEnabled(false);
-               moveViewToScreenCenter(img);
-                selectedTextView.setText("Du har valgt : "+((CircleImageView) view).getName());
-                moveViewToScreenCenter(selectedTextView);
-                setTxtAndRotate(tidLaese,svaerhedsgrad);
                 circleLayout.setRotating(false);
 
-                seekBar.setVisibility(VISIBLE);
-                rotateSeekBar(seekBar);
+                moveViewToScreenCenter(img_his);
+                moveViewToScreenCenter(selectedTextView);
 
-                seekBar2.setVisibility(VISIBLE);
-                rotateSeekBar(seekBar2);
+                rotateSeekBar(seekBar,seekBar2);
 
-                las.setVisibility(VISIBLE);
-               // selectedTextView.setText("");
+                initializeOnClick (view);
 
-
-
+                setTextViewRotate(tidLaese,svaerhedsgrad,textView,textView2);
 
                 break;
             case R.id.krimi:
+                ImageView img_krimi = (ImageView) findViewById(R.id.krimi);
+
+                circleLayout.setEnabled(false);
+                circleLayout.setRotating(false);
+
+                circleLayout.removeViewAt(5);
+                circleLayout.removeViewAt(4);
+                circleLayout.removeViewAt(3);
+                circleLayout.removeViewAt(2);
+                circleLayout.removeViewAt(0);
+
+                circleLayout.setFirstChildPosition(CircleLayout.FirstChildPosition.SOUTH);
+
+                moveViewToScreenCenter(img_krimi);
+
+                moveViewToScreenCenter(selectedTextView);
+
+                rotateSeekBar(seekBar,seekBar2);
+
+                initializeOnClick (view);
+
+                setTextViewRotate(tidLaese,svaerhedsgrad,textView,textView2);
+
+
 
                 break;
             case R.id.mad:
 
+                ImageView img_mad = (ImageView) findViewById(R.id.mad);
+
+                circleLayout.setEnabled(false);
+                circleLayout.setRotating(false);
+
+                circleLayout.removeViewAt(5);
+                circleLayout.removeViewAt(4);
+                circleLayout.removeViewAt(3);
+                circleLayout.removeViewAt(1);
+                circleLayout.removeViewAt(0);
+
+                circleLayout.setFirstChildPosition(CircleLayout.FirstChildPosition.SOUTH);
+
+                moveViewToScreenCenter(img_mad);
+
+                moveViewToScreenCenter(selectedTextView);
+
+                rotateSeekBar(seekBar,seekBar2);
+
+                initializeOnClick (view);
+
+                setTextViewRotate(tidLaese,svaerhedsgrad,textView,textView2);
                 break;
             case R.id.natur:
+                ImageView img_natur = (ImageView) findViewById(R.id.natur);
 
+                circleLayout.setEnabled(false);
+                circleLayout.setRotating(false);
+
+                circleLayout.removeViewAt(5);
+                circleLayout.removeViewAt(4);
+                circleLayout.removeViewAt(2);
+                circleLayout.removeViewAt(1);
+                circleLayout.removeViewAt(0);
+
+                circleLayout.setFirstChildPosition(CircleLayout.FirstChildPosition.SOUTH);
+
+                moveViewToScreenCenter(img_natur);
+
+                moveViewToScreenCenter(selectedTextView);
+
+                rotateSeekBar(seekBar,seekBar2);
+
+                initializeOnClick (view);
+
+                setTextViewRotate(tidLaese,svaerhedsgrad,textView,textView2);
                 break;
             case R.id.sport:
+                ImageView img_sport = (ImageView) findViewById(R.id.sport);
 
+                circleLayout.setEnabled(false);
+                circleLayout.setRotating(false);
+
+                circleLayout.removeViewAt(5);
+                circleLayout.removeViewAt(3);
+                circleLayout.removeViewAt(2);
+                circleLayout.removeViewAt(1);
+                circleLayout.removeViewAt(0);
+
+                circleLayout.setFirstChildPosition(CircleLayout.FirstChildPosition.SOUTH);
+
+                moveViewToScreenCenter(img_sport);
+
+                moveViewToScreenCenter(selectedTextView);
+
+                rotateSeekBar(seekBar,seekBar2);
+
+                initializeOnClick (view);
+
+                setTextViewRotate(tidLaese,svaerhedsgrad,textView,textView2);
                 break;
             case R.id.random:
+                ImageView img_random = (ImageView) findViewById(R.id.random);
 
+                circleLayout.setEnabled(false);
+                circleLayout.setRotating(false);
+
+                circleLayout.removeViewAt(4);
+                circleLayout.removeViewAt(3);
+                circleLayout.removeViewAt(2);
+                circleLayout.removeViewAt(1);
+                circleLayout.removeViewAt(0);
+
+                circleLayout.setFirstChildPosition(CircleLayout.FirstChildPosition.SOUTH);
+
+                moveViewToScreenCenter(img_random);
+
+                moveViewToScreenCenter(selectedTextView);
+
+                rotateSeekBar(seekBar,seekBar2);
+
+                initializeOnClick (view);
+
+                setTextViewRotate(tidLaese,svaerhedsgrad,textView,textView2);
                 break;
         }
 
@@ -238,7 +346,7 @@ public class MainActivity extends AppCompatActivity implements CircleLayout.OnIt
             params.setMargins(0, 0, 0, 0); //substitute parameters for left, top, right, bottom
             view.setLayoutParams(params);
         }
-
+        circleLayout.setFirstChildPosition(CircleLayout.FirstChildPosition.SOUTH);
         RelativeLayout root = (RelativeLayout) findViewById( R.id.rootLayout);
         DisplayMetrics dm = new DisplayMetrics();
         this.getWindowManager().getDefaultDisplay().getMetrics( dm );
@@ -259,28 +367,63 @@ public class MainActivity extends AppCompatActivity implements CircleLayout.OnIt
         view.startAnimation(anim);
     }
 
-    private void rotateSeekBar(SeekBar s){
-        s.setDrawingCacheEnabled(true);
-        Animation animation = new RotateAnimation(200, 360, s.getWidth() , s.getHeight() );
-        animation.setDuration(1250);
-        s.startAnimation(animation);
+    private void rotateSeekBar(SeekBar... s){
+        for(int i = 0 ; i < s.length;i++) {
+            SeekBar SeekBarAraay = s[i];
+            s[i].setDrawingCacheEnabled(true);
+            Animation animation = new RotateAnimation(200, 360, s[i].getWidth(), s[i].getHeight());
+            animation.setDuration(1250);
+            s[i].startAnimation(animation);
+        }
     }
 
-    private void rotateTextView(TextView t){
-        t.setDrawingCacheEnabled(true);
-        Animation animation = new RotateAnimation(200, 360, t.getWidth() , t.getHeight() );
-        animation.setDuration(1250);
-        t.startAnimation(animation);
+
+    private  void setTextViewRotate( TextView... t){
+
+        for(int i = 0 ; i < t.length;i++) {
+            TextView textViewAraay = t[i];
+             Animation animation = new RotateAnimation(200, 360, t[i].getWidth(), t[i].getHeight());
+            animation.setDuration(1250);
+            t[i].startAnimation(animation);
+
+        }
     }
 
-    private  void setTxtAndRotate( TextView t, TextView t1){
+    private  void initializeOnClick (View v){
+
         tidLaese.setText("Hvor lang tid vil du læse? (I minutter)");
+
         svaerhedsgrad.setText("Hvilken særhedsgrad skal teksten have?");
-        t.setDrawingCacheEnabled(true);
-        Animation animation = new RotateAnimation(200, 360, t.getWidth() , t.getHeight() );
+
+        selectedTextView.setText("Du har valgt : "+((CircleImageView) v).getName());
+
+        textView.setText("Du har valgt "+String.valueOf(seekBar.getProgress())+" Minutter");
+
+
+        switch(seekBar2.getProgress()){
+            case 0:
+                textView2.setText("Du har valgt : Let øvet");
+                break;
+            case 1:
+                textView2.setText("Du har valgt : Øvet");
+                break;
+            case 2:
+                textView2.setText("Du har valgt : Meget øvet");
+                break;
+            case 3:
+                textView2.setText("Du har valgt : Ekspert");
+                break;
+            default:
+                break;
+        }
+        seekBar.setVisibility(VISIBLE);
+        seekBar2.setVisibility(VISIBLE);
+        las.setVisibility(VISIBLE);
+        las.setText("LÆS!");
+        // animation for knappen
+        Animation animation = new RotateAnimation(200, 360, las.getWidth(), las.getHeight());
         animation.setDuration(1250);
-        t.startAnimation(animation);
-        t1.startAnimation(animation);
+        las.startAnimation(animation);
     }
 
 }
